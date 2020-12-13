60bf87b64a41cae17ce7b4874d85947f1385ade2
Darshan Datta <darshandatta.10@gmail.com>
2020-12-14 00:55:43 +0530

    1. added react router dom 2. loaded basic home page with Route 3. added react lazy and suspense

--- src/features/app/Root.js
+++ src/features/app/Root.js
@@ -1,19 +1,36 @@
-import { Container, Button } from "@material-ui/core";
+import { Container, CircularProgress } from "@material-ui/core";
+import { lazy, Suspense } from "react";
 import Navbar from "./Navbar";
+import { Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Root = () => (
-  <>
-    <Navbar />
-    <Container component="main">
-      <div>Hello world</div>
-      <Button variant="contained" color="primary">
-        Primary Button
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary Button
-      </Button>
-    </Container>
-  </>
-);
+const useStyles = makeStyles({
+  main: {
+    minHeight: "100vh",
+  },
+  spinner: {
+    margin: "10% 50%",
+  },
+});
+
+const Home = lazy(() => import("../home"));
+
+const Root = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Navbar />
+      <Container className={classes.main} component="main">
+        <Suspense
+          fallback={
+            <CircularProgress className={classes.spinner} color="secondary" />
+          }
+        >
+          <Route path="/" component={Home} />
+        </Suspense>
+      </Container>
+    </>
+  );
+};
 
 export default Root;
---
+++ src/features/home/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 style={{ marginTop: 70 }}>Home Page</h1>;
+}
